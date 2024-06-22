b43a01099df2c85fcb5df3963fa3c221ec8f2005
Mohammad Abu Obaida Mullick <86158154+MAOMislive@users.noreply.github.com>
2024-06-22 23:01:53 +0600

    Update batch_rename_images.py

--- batch_rename_images.py
+++ batch_rename_images.py
@@ -1,9 +1,10 @@
 import os
 
+
 def rename_files(input_folder):
     # Get all files in the input folder
     files = os.listdir(input_folder)
-    
+
     # Sort files to ensure consistent ordering
     files.sort()
 
@@ -11,14 +12,17 @@ def rename_files(input_folder):
     for i, file in enumerate(files):
         if os.path.isfile(os.path.join(input_folder, file)):
             # Construct new name
-            new_name = f"ODM_{i+1}"
+            new_name = f"ODM_{i + 1}.png"
 
             # Rename file
             os.rename(os.path.join(input_folder, file), os.path.join(input_folder, new_name))
 
             print(f"Renamed {file} to {new_name}")
 
+
 # Example usage
-input_folder = r'C:\\location\to\the\folder'
+input_folder = r'C:\Users\User\Desktop\Iamges'
 
 rename_files(input_folder)
+
+print('Done Renaming!')
