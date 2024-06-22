bc8f5bd960449acf7b95c99a45182712a225f2cc
Mohammad Abu Obaida Mullick <86158154+MAOMislive@users.noreply.github.com>
2024-06-22 23:00:13 +0600

    Update PNG_to_JPEG.py

--- PNG_to_JPEG.py
+++ PNG_to_JPEG.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 from PIL import Image
 
-inputPath = Path(r"C:\Users\PNG") # PNG(Source) Folder Location
+inputPath = Path(r"C:\Users\User\Desktop\Images\PNG")
 inputFiles = inputPath.glob("**/*.png")
-outputPath = Path(r"C:\Users\JPEG") # JPEG(Destination) Folder Location
+outputPath = Path(r"C:\Users\User\Desktop\Images\JPEG")
 
 for f in inputFiles:
     outputFile = outputPath / Path(f.stem + ".jpg")
@@ -11,3 +11,6 @@ for f in inputFiles:
     if im.mode == 'RGBA':
         im = im.convert('RGB')
     im.save(outputFile)
+
+
+print('Done Converting!')
